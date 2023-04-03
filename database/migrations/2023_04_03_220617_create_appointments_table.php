43e6db808d7b568a97e386bf1d1aa70cf46f0732
issam <issammez44@gmail.com>
2023-04-03 22:31:43 +0000

    add appointments migration

---
+++ database/migrations/2023_04_03_220617_create_appointments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date');
+            $table->float('quantity');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('blood_drive_id')->nullable()->constrained();
+            $table->foreignId('hospital_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
